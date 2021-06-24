cd266a8fa57ca6e11e1c9cded4729f8054da478f
Justin Shaber <justinshaber@gmail.com>
2021-06-23 23:18:07 -0700

    Exercises

---
+++ ruby_basics/loops/loops5.rb
@@ -0,0 +1,7 @@
+say_hello = 1
+
+while say_hello <= 5
+  puts 'Hello!'
+
+  say_hello += 1
+end
---
+++ ruby_basics/loops/loops6.rb
@@ -0,0 +1,9 @@
+numbers = []
+count = 0
+
+while count < 5
+  numbers << rand(99)
+  count += 1
+end
+
+puts numbers
