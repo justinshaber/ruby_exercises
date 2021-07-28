f60d9c6a85dc5f7f94ca72ee36e8aa8195398600
Justin Shaber <justinshaber@gmail.com>
2021-07-27 18:06:50 -0700

    More exercises

---
+++ rb_101_small_problems/easy_4/century.rb
@@ -0,0 +1,28 @@
+1st
+2nd
+3rd
+4th
+5th
+6th
+7th
+8th
+9th
+10th
+11th
+12th
+13th
+14th, 15th 16th 17th 19th 20th
+
+def century(year)
+  
+end
+
+century(2000) == '20th'
+century(2001) == '21st'
+century(1965) == '20th'
+century(256) == '3rd'
+century(5) == '1st'
+century(10103) == '102nd'
+century(1052) == '11th'
+century(1127) == '12th'
+century(11201) == '113th'
